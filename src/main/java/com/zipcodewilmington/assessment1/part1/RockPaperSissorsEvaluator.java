2ab2b996604440d383d00ae663bf1419e5c7db96
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-02-17 10:15:10 -0500

    finished more labs

--- src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
@@ -13,7 +13,13 @@ public class RockPaperSissorsEvaluator {
      * @return the respective winning move
      */
     public String getWinningMove(String handSign) {
-        return null;
+        if(handSign.equals("rock")){
+            return "paper";
+        }
+        else if(handSign.equals("paper")){
+            return "scissor";
+        }
+        return "rock";
     }
 
     /**
@@ -21,7 +27,13 @@ public class RockPaperSissorsEvaluator {
      * @return the respective losing move
      */
     public String getLosingMove(String handSign) {
-        return null;
+        if(handSign.equals("scissor")){
+            return "paper";
+        }
+        else if(handSign.equals("paper")){
+            return "rock";
+        }
+        return "scissor";
     }
 
     /**
@@ -30,6 +42,12 @@ public class RockPaperSissorsEvaluator {
      * @return a string representative of the winning hand sign between the two players
      */
     public String getWinner(String handSignOfPlayer1, String handSignOfPlayer2) {
+        if(getWinningMove(handSignOfPlayer1).equals(handSignOfPlayer2)){
+            return handSignOfPlayer2;
+        }
+        else if(getWinningMove(handSignOfPlayer2).equals(handSignOfPlayer1)){
+            return handSignOfPlayer1;
+        }
         return null;
     }
 }
